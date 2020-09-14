78eee3b316965d827c652030c6557b253d548d6a
Emmanuel <ortizdeluna74@gmail.com>
2020-09-14 15:05:06 -0500

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,84 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  preguntas: any;
+  posicion = 0;
+  terminado=false;
+  TomarFoto = false;
+  HabilitarSiguiente= true;
+  constructor(private camera: Camera) {
+    this.preguntas = [{
+      articulo: 'Faro',
+      respuesta: '',
+      comentario: '',
+      imagen: ''
+      
+    },
+    {
+      articulo: 'Parabrisas',
+      respuesta: '',
+      comentario: '',
+      imagen: ''
+    },
+    {
+      articulo: 'Antena',
+      respuesta: '',
+      comentario: '',
+      imagen: ''
+    },
+    {
+      articulo: 'Retrovisor',
+      respuesta: '',
+      comentario: '',
+      imagen: ''
+    }]
+  }
+  siguiente(){
+    let tope = this.preguntas.length - 1;
+    console.log(tope);
+
+    if(this.posicion === tope){
+    console.log('1')
+    this.terminado = true;
+    }else{
+      console.log('2')
+
+      this.posicion = this.posicion + 1;
+      this.HabilitarSiguiente = true;
+
+    }
+  }
+  anterior(){
+    this.posicion = this.posicion - 1;
+
+  }
+  enviar(){
+    console.log(this.preguntas);
+  }
+  checarOpcion(){
+    console.log('hola');
+    this.HabilitarSiguiente = false;
+  }
+  async hacerFoto() {
+    
+    const options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
+    }
+    const result = await this.camera.getPicture(options).then((imageData) => {
+      /*this.preguntas[this.posicion].push({
+      imagen: 'data:image/jpeg;base64,' + imageData
+      });  */
+      this.preguntas[this.posicion].imagen = 'data:image/jpeg;base64,' + imageData;
+      //this.imagenes.push(this.base64Image);
+    }, (err) => {
+      console.log(err);
+    });
+    //this.longitudImagenes = this.imagenes.length;
+  
+  }
+
 
 }
